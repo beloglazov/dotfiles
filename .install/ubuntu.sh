c778ec9fa06158ea598fb54e5b2f5836d7f298d7
Anton Beloglazov <anton.beloglazov@gmail.com>
2015-04-21 16:29:20 +1000

    Added vim configuration and ubuntu software installation script

---
+++ .install/ubuntu.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+sudo apt-get -y install \
+    build-essential cmake xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev \
+    suckless-tools vim xfonts-terminus rxvt-unicode python2.7-dev htop tree \
+    dzen2 ranger numlockx xxkb lftp colordiff ncdu feh pydf cloc zathura dtrx \
+    fish exuberant-ctags silversearcher-ag chromium-browser
