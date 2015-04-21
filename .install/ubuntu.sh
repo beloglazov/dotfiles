859c76c2ecc31af6caf5ef1d87aa040c4bea1b48
Anton Beloglazov <anton.beloglazov@gmail.com>
2015-04-21 20:51:02 +1000

    Added fzf

--- .install/ubuntu.sh
+++ .install/ubuntu.sh
@@ -4,4 +4,4 @@ sudo apt-get -y install \
     build-essential cmake xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev \
     suckless-tools vim xfonts-terminus rxvt-unicode python2.7-dev htop tree \
     dzen2 ranger numlockx xxkb lftp colordiff ncdu feh pydf cloc zathura dtrx \
-    fish exuberant-ctags silversearcher-ag chromium-browser
+    fish exuberant-ctags silversearcher-ag chromium-browser ruby golang
