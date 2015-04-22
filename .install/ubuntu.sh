5ad25e5fbf16dd3897720f943af14493d4474d63
Anton Beloglazov <anton.beloglazov@gmail.com>
2015-04-22 15:00:43 +1000

    Added rxvt-unicode-256color and fish 2.1.2

--- .install/ubuntu.sh
+++ .install/ubuntu.sh
@@ -4,14 +4,24 @@ sudo add-apt-repository -y ppa:russell-s-stewart/ppa
 sudo apt-get update
 sudo apt-get -y install \
     build-essential cmake xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev \
-    suckless-tools vim xfonts-terminus rxvt-unicode python2.7-dev htop tree \
+    suckless-tools vim xfonts-terminus rxvt-unicode-256color python2.7-dev \
     dzen2 ranger numlockx xxkb lftp colordiff ncdu feh pydf cloc zathura dtrx \
     fish exuberant-ctags silversearcher-ag chromium-browser ruby golang sshrc \
-    libxtst-dev wmctrl
+    libxtst-dev wmctrl autoconf libncurses5-dev htop tree
 
 cd /tmp
 rm -rf xcape
 git clone https://github.com/alols/xcape.git
 cd xcape
+make
+sudo make install
+
+cd /tmp
+rm -rf fish-shell
+git clone git@github.com:fish-shell/fish-shell.git
+cd fish-shell
+git checkout 2.1.2
+autoconf
+./configure
 make
 sudo make install
