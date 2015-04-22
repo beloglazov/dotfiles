3abf7caec44b16b181a24b85db8d5c1754ce5bd4
Anton Beloglazov <anton.beloglazov@gmail.com>
2015-04-22 15:38:03 +1000

    Removed urxvt-extensions from .install/ubuntu.sh

--- .install/ubuntu.sh
+++ .install/ubuntu.sh
@@ -5,10 +5,10 @@ sudo apt-add-repository -y ppa:fish-shell/release-2
 sudo apt-get update
 sudo apt-get -y install \
     build-essential cmake xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev \
-    suckless-tools vim xfonts-terminus rxvt-unicode-256color urxvt-extensions \
+    suckless-tools vim xfonts-terminus rxvt-unicode-256color python2.7-dev \
     dzen2 ranger numlockx xxkb lftp colordiff ncdu feh pydf cloc zathura dtrx \
     fish exuberant-ctags silversearcher-ag chromium-browser ruby golang sshrc \
-    libxtst-dev wmctrl autoconf libncurses5-dev htop tree python2.7-dev
+    libxtst-dev wmctrl autoconf libncurses5-dev htop tree
 
 cd /tmp
 rm -rf xcape
