249febc1fbe6794425e269da42ac3a405dc68e07
Anton Beloglazov <anton.beloglazov@gmail.com>
2015-04-22 12:25:45 +1000

    Added sshrc to .install/ubuntu, updated the ssh fish function to handle the absence of sshrc

--- .install/ubuntu.sh
+++ .install/ubuntu.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env sh
 
+sudo add-apt-repository ppa:russell-s-stewart/ppa
+sudo apt-get update
 sudo apt-get -y install \
     build-essential cmake xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev \
     suckless-tools vim xfonts-terminus rxvt-unicode python2.7-dev htop tree \
     dzen2 ranger numlockx xxkb lftp colordiff ncdu feh pydf cloc zathura dtrx \
-    fish exuberant-ctags silversearcher-ag chromium-browser ruby golang
+    fish exuberant-ctags silversearcher-ag chromium-browser ruby golang sshrc
--- tag-shell/config/fish/functions/ssh.fish
+++ tag-shell/config/fish/functions/ssh.fish
@@ -1,4 +1,13 @@
 function ssh
-    command sshrc $argv
+    which sshrc >- ^-
+    set sshrc_exists $status
+    if test $sshrc_exists -eq 0 -a (count $argv) -eq 1
+        command sshrc $argv
+    else
+        if test $sshrc_exists -ne 0
+            echo Warning: sshrc is not installed, falling back to ssh
+        end
+        command ssh $argv
+    end
 end
 
