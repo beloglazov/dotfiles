f2094dee920713d89358abdba86a2b36aae93cc0
Anton Beloglazov <anton.beloglazov@gmail.com>
2015-04-22 15:14:19 +1000

    Switched to using the fish ppa

--- .install/ubuntu.sh
+++ .install/ubuntu.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
-sudo add-apt-repository -y ppa:russell-s-stewart/ppa
+sudo apt-add-repository -y ppa:russell-s-stewart/ppa
+sudo apt-add-repository -y ppa:fish-shell/release-2
 sudo apt-get update
 sudo apt-get -y install \
     build-essential cmake xmonad libghc-xmonad-dev libghc-xmonad-contrib-dev \
@@ -13,15 +14,5 @@ cd /tmp
 rm -rf xcape
 git clone https://github.com/alols/xcape.git
 cd xcape
-make
-sudo make install
-
-cd /tmp
-rm -rf fish-shell
-git clone git@github.com:fish-shell/fish-shell.git
-cd fish-shell
-git checkout 2.1.2
-autoconf
-./configure
 make
 sudo make install
