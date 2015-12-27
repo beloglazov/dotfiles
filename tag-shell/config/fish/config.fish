ceb28f947cb2c6df675f1d3643184c98db9d40dc
Anton Beloglazov <anton.beloglazov@gmail.com>
2015-12-27 13:52:51 -0800

    Updated the fish config with key bindings and abbreviations

--- tag-shell/config/fish/config.fish
+++ tag-shell/config/fish/config.fish
@@ -1,58 +1,65 @@
-# Path to oh-my-fish
-set fish_path $HOME/.oh-my-fish
-
-# Correct default editor
+# The correct default editor
 set --export EDITOR vim
 
-# Vi-mode key bindings
-function vi_mode_user -a mode
+# Key bindings
+function fish_user_key_bindings
+    fish_vi_key_bindings
 
-    bind \cu 'pushd ..; commandline -f repaint'
-    bind \cf 'popd >/dev/null; commandline -f repaint'
-    bind \cn 'cd -; commandline -f repaint'
-    bind \cb 'commandline -r ""; and commandline -f "execute" "history-search-backward"'
+    # Exit the insert mode on C-Q
+    bind -M insert -m default \cq force-repaint
 
-    # Base bindings
+    # History search
     bind \ck up-or-search
+    bind -M insert \ck up-or-search
+
+    # Basic movement
     bind \ch backward-char
     bind \cl forward-char
+    bind -M insert \ch backward-char
+    bind -M insert \cl forward-char
+    bind h beginning-of-line
+    bind l end-of-line
+
+    # Directory navigation
+    bind -M insert \cu 'pushd ..; commandline -f repaint'
+    bind -M insert \cf 'popd >/dev/null; commandline -f repaint'
+    bind -M insert \cn 'cd -; commandline -f repaint'
 
     # fzf bindings
-    bind \cr 'fzf-history; commandline -f repaint'
-    bind \cy 'fzf-vim-fasd; commandline -f repaint'
-    bind \cp 'fzf-vim-subtree; commandline -f repaint'
-    bind \cs 'fzf-cd-subtree; commandline -f repaint'
-    bind \cx 'fzf-cd-fasd; commandline -f repaint'
-
-    # Mode-specific bindings
-    switch $mode
-        case normal
-            bind h beginning-of-line
-            bind l end-of-line
-        case insert
-            bind \cq vi_mode_normal
-            # bind \cj down-or-search
-    end
+    bind -M insert \cr 'fzf-history; commandline -f repaint'
+    bind -M insert \cy 'fzf-vim-fasd; commandline -f repaint'
+    bind -M insert \cp 'fzf-vim-subtree; commandline -f repaint'
+    bind -M insert \cs 'fzf-cd-subtree; commandline -f repaint'
+    bind -M insert \cx 'fzf-cd-fasd; commandline -f repaint'
 end
 
-# Disable the welcome text
-set --erase fish_greeting
-
-# Disable GREP_OPTIONS
-set --erase GREP_OPTIONS
-
-# Load oh-my-fish configuration
-. $fish_path/oh-my-fish.fish
-
-# Theme
-Theme "beloglazov"
-
-# Plugins
-Plugin "vi-mode"
-Plugin "pbcopy"
+# Abbreviations
+abbr -a as apt search
+abbr -a bc bc -l
+abbr -a diff colordiff
+abbr -a f 'find . | ag --smart-case'
+abbr -a fd 'find . -type d | ag --smart-case'
+abbr -a g ag --smart-case
+abbr -a ga git add
+abbr -a gb git branch
+abbr -a gco git checkout
+abbr -a gd git diff
+abbr -a gp git push
+abbr -a gs git status -sb
+abbr -a gu git pull --rebase
+abbr -a l ls -lh
+abbr -a ll ls -alh
+abbr -a lls ls -alhSr
+abbr -a lt ls -alhtr
+abbr -a mkdirs mkdir -p
+abbr -a psg 'ps aux | ag --smart-case'
+abbr -a view vim -R -
+abbr -a x dtrx
 
 # Add fzf to PATH
 set PATH ~/.bin $PATH ~/.fzf
 
 # Add extra PATH components
-. ~/.config/fish/path*.fish
+for file in ~/.config/fish/path*.fish
+    . $file
+end
